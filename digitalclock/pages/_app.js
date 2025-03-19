2574c348b9164a6ba031e2c7e20f2eb5387aa90f
Ruby-Franco <rubyfranco.1@gmail.com>
2025-03-18 22:41:21 -0700

    add basic basic design and favIcon integration

--- digitalclock/pages/_app.js
+++ digitalclock/pages/_app.js
@@ -1,5 +1,15 @@
+import React from "react";
+import Head from "next/head";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Head> 
+        {"http://www.w3.org/1999/xlink"}
+        <link rel= "icon" href="/favicon.svg" type="image/svg+xml" />
+    </Head>
+    <Component {...pageProps} />
+    </>
+  );
 }
---
+++ digitalclock/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './pages/**/*.{js,jsx,ts,tsx}', // Watch all files in the pages folder
+      './components/**/*.{js,jsx,ts,tsx}', // Watch all components (if any)
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
