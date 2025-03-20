4ff29b5dee2cc9bd9beac145f8243f71e0fe0d22
Ruby-Franco <rubyfranco.1@gmail.com>
2025-03-19 22:40:21 -0700

    fairy lights animation + neon clock display animation

--- digitalclock/pages/_app.js
+++ digitalclock/pages/_app.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Head from "next/head";
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+
 
 export default function App({ Component, pageProps }) {
   return (
     <>
     <Head> 
-        {"http://www.w3.org/1999/xlink"}
+    <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+        />
         <link rel= "icon" href="/favicon.svg" type="image/svg+xml" />
     </Head>
     <Component {...pageProps} />
--- digitalclock/pages/index.js
+++ digitalclock/pages/index.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 export default function DigitalClock() {
 
   const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString()); 
+
 
   useEffect( ()=> {
     const updateTime = setInterval( () => {setCurrentTime(new Date().toLocaleTimeString());
@@ -16,12 +17,29 @@ export default function DigitalClock() {
   if (currentTime === null) {
     return <div>Loading...</div>;
   }
-
   
   return (
-    <div className=" items-center min-h-screen bg-gray-800 text-white"> 
-      <header className="font-bold text-4xl mb-4"> Digital Clock </header> 
-      <p className="text-6xl">{currentTime} </p>
+    <div className=" fairy-lights-container items-center min-h-screen text-white ambient-wall "> 
+      <div className="fairy-lights">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <div
+            key={index}
+            className="fairy-light"
+            style={{
+              left: `${index * 10}%`, //`${xPos}%`,//
+              top: `${index^2 + 7}%`,//`${yPos}px`, 
+              animationDelay: `${index * 0.2}s`,
+            }}
+          />
+          ))}
+      </div>
+
+
+      <div className="clock-container">
+        <p className="clock-display text-2xl ">{currentTime} </p>
+      </div>
+      
+      
     </div>
   );
 }
